1179fb9e4debe3ec3196235d2e23629285f43c4b
Travon Billups <travonbillups@yahoo.com>
2017-12-10 17:03:00 -0600

    made a simple age verification program

--- csharp2/Program.cs
+++ csharp2/Program.cs
@@ -10,6 +10,25 @@ namespace csharp2
     {
         static void Main(string[] args)
         {
+            int userAge;
+
+            Console.Write("Please enter your age: ");
+            userAge = Convert.ToInt32(Console.ReadLine());
+
+            if (userAge < 0 || userAge > 100)
+            {
+                Console.WriteLine("Invalid Age");
+                Console.WriteLine("Age must be between 0 and 100");
+            }
+            else if (userAge < 18)
+                Console.WriteLine("Sorry you are underage");
+            else if (userAge < 21)
+                Console.WriteLine("You need parental consent");
+            else
+            {
+                Console.WriteLine("Congradulations!");
+                Console.WriteLine("You may sign up for the event!");
+            }
         }
     }
 }
